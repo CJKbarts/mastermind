f0813f9e373d2a60f7001a7692946f6791790c4e
CJKbarts <bartelsjacobcorn@gmail.com>
2024-05-09 10:20:39 -0500

    Add display.rb file
    Add Display module
    Add functions that print out information to the
    console
    Prevents crowding important file with large blocks
    of text

---
+++ display.rb
@@ -0,0 +1,27 @@
+module Display
+  def welcome
+    puts "Welcome To Mastermind"
+    puts
+  end
+
+  def rules
+    puts "Computer randomly picks a sequence of 4 colors from the colors:"
+    puts "Red, Orange, Yellow, Green, Blue, Indigo, Violet, Pink"
+    puts
+
+    puts "To beat the game, players must guess the color using 6 tries"
+    puts "After each guess you would be informed of the number of black ball and white balls"
+    puts "Number of black balls shows how many colors were guessed right and are in the right spot"
+    puts "Number of black balls shows how many colors were guessed right but are in the wrong spot"
+    puts
+
+    puts "Enter your guesses as a string of the initials of the colors in the sequence"
+    puts "START"
+    puts
+  end
+
+  def guess_feedback(array)
+    puts "Black balls: #{array[0]}"
+    puts "White balls: #{array[1]}"
+  end
+end
