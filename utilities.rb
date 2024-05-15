c2918534b86a791cae01c62f6fec24d8e5aef00f
CJKbarts <bartelsjacobcorn@gmail.com>
2024-05-15 16:44:34 -0500

    Change display_feedback to display_history
    display_history shows a list of code_breakers previous guesses.
    Allow code_breaker to analyze past guesses and responses easily to make
    the next guess

--- utilities.rb
+++ utilities.rb
@@ -5,24 +5,35 @@ module Utilities
   end
 
   def display_rules
-    puts "Computer randomly picks a sequence of 4 colors from the colors:"
-    puts "Red, Orange, Yellow, Green, Blue, Indigo, Violet, Pink"
+    puts "Rules of the Game"
+    puts "Code maker create a sequence of 4 colors from the colors:"
+    puts "Red, Orange, Yellow, Green, Blue, Indigo"
     puts
 
-    puts "To beat the game, players must guess the color using 6 tries"
-    puts "After each guess you would be informed of the number of black ball and white balls"
-    puts "Number of black balls shows how many colors were guessed right and are in the right spot"
-    puts "Number of black balls shows how many colors were guessed right but are in the wrong spot"
+    puts "To beat the game, code breakers must guess the color using 6 tries"
+    puts "After each guess,code breakers would be informed of the number of " +
+         "black and white balls they obtained"
+    puts "Black balls show how many colors were guessed right and are in the right spot"
+    puts "White balls show how many colors were guessed right but are in the wrong spot"
     puts
 
-    puts "Enter your guesses as a string of the initials of the colors in the sequence"
+    puts "Enter your codes and guesses as strings of the initials of the colors in the sequence"
+    puts "Example: ROYG would be a sequence of Red, Orange, Yellow, and Green"
     puts "START"
     puts
   end
 
-  def display_feedback(array)
-    puts "Black balls: #{array[0]}"
-    puts "White balls: #{array[1]}"
+  def display_history(guess_history)
+    guess_history.each do |guess, feedback|
+      print "#{guess} - "
+      display_feedback(feedback)
+      puts
+    end
+  end
+
+  def display_feedback(feedback)
+    feedback[0].times { print "B" }
+    feedback[1].times { print "W" }
   end
 
   def get_input(prompt)
